a39d1437048d052c4b6603365c36f6e0a605b025
Giulio Astori <g.astori@outlook.com>
2022-05-10 13:09:44 -0400

    5/10/22

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -1,10 +1,20 @@
-$TLS12Protocol = [System.Net.SecurityProtocolType] 'Ssl3 , Tls12'
-$version = "4.8"
-$location = "https://dotnet.microsoft.com/en-us/download/dotnet-framework/thank-you/net48-offline-installer"
-$exePath = "$env:TEMP\$($version)-net-runtime.exe"
-
-[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
-Update-MpSignature
-(New-Object Net.WebClient).DownloadFile($location, $exePath)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -name NuGet -Force
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module PSWindowsUpdate
+Get-WindowsUpdate
+Get-WindowsUpdate -IgnoreReboot
+Install-WindowsUpdate -AcceptAll -IgnoreReboot
+$url = "https://dotnet.microsoft.com/en-us/download/dotnet-framework/thank-you/net48-offline-installer"
+$localFolder="c:\dotnet"
+New-Item -Path $localFolder -ItemType Directory
+$exePath = "c:\dotnet\4.8-net-runtime.exe"
+$Wget = New-Object System.Net.WebClient
+$Wget.DownloadFile($url, $exePath)
 cmd /c start /wait "$exePath" /q /norestart
 Remove-Item $exePath -Force -ErrorAction Ignore
+Remove-Item $localFolder -Force -ErrorAction Ignore
+
+
+
+
