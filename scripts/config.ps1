dfa5e47ad7d880bf8184c64a45b712cb7a054cc4
Giulio Astori <g.astori@outlook.com>
2022-05-10 15:29:35 -0400

    5/10/22

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -1,13 +1,25 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://go.microsoft.com/fwlink/?linkid=2088631"
-$localFolder="c:\dotnet"
-New-Item -Path $localFolder -ItemType Directory
-$exePath = "c:\dotnet\4.8-net-runtime.exe"
-$Wget = New-Object System.Net.WebClient
-$Wget.DownloadFile($url, $exePath)
-cmd /c start /wait "$exePath" /q /norestart
-Remove-Item $exePath -Force -ErrorAction Ignore
-Remove-Item $localFolder -Force -ErrorAction Ignore
+
+$url0 = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/defu/2022/04/updateplatform_4ca3e501a402a6d913024fdb532c0750152ffb12.exe"
+$localFolder0="c:\defupdate"
+New-Item -Path $localFolder0 -ItemType Directory
+$exePath0 = "c:\defupdate\defupdate.exe"
+$Wget0 = New-Object System.Net.WebClient
+$Wget0.DownloadFile($url0, $exePath0)
+cmd /c start /wait "$exePath0" /q /norestart
+Remove-Item $exePath0 -Force -ErrorAction Ignore
+Remove-Item $localFolder0 -Force -ErrorAction Ignore
+
+
+$url1 = "https://go.microsoft.com/fwlink/?linkid=2088631"
+$localFolder1="c:\dotnet"
+New-Item -Path $localFolder1 -ItemType Directory
+$exePath1 = "c:\dotnet\4.8-net-runtime.exe"
+$Wget1 = New-Object System.Net.WebClient
+$Wget1.DownloadFile($url1, $exePath1)
+cmd /c start /wait "$exePath1" /q /norestart
+Remove-Item $exePath1 -Force -ErrorAction Ignore
+Remove-Item $localFolder1 -Force -ErrorAction Ignore
 
 
 
